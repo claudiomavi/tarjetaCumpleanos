362b46fe54138cd3cbabf45903646395b6385fb7
claudiomavi <118379948+claudiomavi@users.noreply.github.com>
2025-03-26 21:17:24 +0100

    Update datas.js

--- src/datas/datas.js
+++ src/datas/datas.js
@@ -4,7 +4,7 @@ export const location = 'Ristorante La Bella Vita, Roma'
 export const googleMapsLink = 'https://www.google.com/maps?q=Ristorante+La+Bella+Vita,+Roma'
 const phoneNumber = '393917647207' // Código de país + número sin espacios
 const whatsappMessage = encodeURIComponent(
-	`Ciao ${name}! Confermo la mia presenza alla tua festa 🎉`
+	`Ciao ${name}! Confermo la mia presenza alla tua festa`
 )
 export const whatsappLink = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`
 export const dressCode = 'Elegante, nero'
