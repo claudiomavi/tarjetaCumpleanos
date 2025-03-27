339563c2549cae43b28954b88e38fdd3a5d52ce3
Claudio <claudio.marino@outlook.com>
2025-03-27 17:29:57 +0100

    restiling with tailwindcss

--- src/Components/Invitation.jsx
+++ src/Components/Invitation.jsx
@@ -2,6 +2,7 @@ import {
   name,
   date,
   location,
+  time,
   googleMapsLink,
   whatsappLink,
   dressCode,
@@ -9,34 +10,71 @@ import {
 
 export default function Invitation() {
   return (
-    <div className="">
-      <div className=""></div>
-
-      <div className="">
-        <h1 className="m-2 p-4 text-blue-500">18</h1>
-        <p className="">Sei invitato/a al diciottesimo di</p>
-        <h2 className="">{name}</h2>
-        <p className="">📅 {date}</p>
-        <p className="">📍 {location}</p>
-        <p className="">🎩 Dress Code: {dressCode}</p>
-        <div className="">
-          <a
-            href={whatsappLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className=""
-          >
-            ✅ Conferma su WhatsApp
-          </a>
-          <a
-            href={googleMapsLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className=""
-          >
-            📍 Vedi la posizione
-          </a>
+    <div className="relative flex h-screen flex-col items-center justify-center bg-[url(/assets/background.webp)] bg-cover">
+      <img
+        src="/assets/cocktail-pink.svg"
+        alt="pink cocktail"
+        className="absolute top-0 right-0"
+      />
+      <img
+        src="/assets/cocktail-white.svg"
+        alt="white cocktail"
+        className="absolute top-[28rem] right-0"
+      />
+      <img
+        src="/assets/cocktail-rounded-pink.svg"
+        alt="pink rounded cocktail"
+        className="absolute bottom-8 left-0"
+      />
+      <div className="flex h-screen max-h-[874px] max-w-[402px] flex-col items-center pt-8">
+        <h1 className="text-center font-party text-9xl text-white text-shadow-white">
+          18
+        </h1>
+        <h1 className="pb-8 text-center font-party text-8xl text-pink-300 text-shadow-white">
+          Birthday Party
+        </h1>
+        <h3 className="text-center font-dm text-white uppercase">
+          festeggiamo il compleanno di {name}
+        </h3>
+        <div className="flex items-center justify-center">
+          <h3 className="text-center font-dm text-white uppercase">
+            alle {time} |{" "}
+            <a
+              href={googleMapsLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline transition-all duration-300 text-shadow-white hover:text-pink-300"
+            >
+              {location}
+            </a>
+          </h3>
+          <img src="/assets/pin.svg" alt="position pin" className="w-[3rem]" />
         </div>
+        <div className="mt-12 flex items-center justify-center gap-3.5">
+          <h3 className="text-center font-dm text-white uppercase">
+            {date.day}
+          </h3>
+          <div class="rounded-full p-4 box-shadow-white outline-4 outline-pink-300">
+            <h3 class="text-center font-dm text-4xl text-white uppercase">
+              {date.number}
+            </h3>
+          </div>
+          <h3 className="text-center font-dm text-white uppercase">
+            {date.month}
+          </h3>
+        </div>
+        <h3 className="pt-4 text-center font-dm text-white uppercase">
+          dress code:{" "}
+          <span className="text-pink-300 text-shadow-white">{dressCode}</span>
+        </h3>
+        <a
+          href={whatsappLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          class="mt-20 cursor-pointer rounded-lg px-5 py-4 text-center font-dm text-white uppercase box-shadow-white outline-2 outline-white transition-all duration-300 hover:bg-white hover:text-black"
+        >
+          conferma su whatsapp
+        </a>
       </div>
     </div>
   );
--- src/datas/datas.js
+++ src/datas/datas.js
@@ -1,10 +1,12 @@
-export const name = 'Rosa'
-export const date = 'Sabato, 20 Aprile 2025'
-export const location = 'Ristorante La Bella Vita, Roma'
-export const googleMapsLink = 'https://www.google.com/maps?q=Ristorante+La+Bella+Vita,+Roma'
-const phoneNumber = '393917647207' // Código de país + número sin espacios
+export const name = "Rosa";
+export const date = { day: "sabato", number: 27, month: "ottobre" };
+export const location = "Ristorante La Bella Vita";
+export const time = "20:30";
+export const googleMapsLink =
+  "https://www.google.com/maps?q=Ristorante+La+Bella+Vita,+Roma";
+const phoneNumber = "393917647207"; // Código de país + número sin espacios
 const whatsappMessage = encodeURIComponent(
-	`Ciao ${name}! Confermo la mia presenza alla tua festa 🎉`
-)
-export const whatsappLink = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`
-export const dressCode = 'Elegante, nero'
+  `Ciao ${name}! Confermo la mia presenza alla tua festa 🎉`,
+);
+export const whatsappLink = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`;
+export const dressCode = "Tropicale";
