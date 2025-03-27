be45dbe24ddd36d4b466d01cba6d3569acfa2251
Claudio <claudio.marino@outlook.com>
2025-03-27 15:07:41 +0100

    added prettier for autosorting

--- src/Components/Invitation.jsx
+++ src/Components/Invitation.jsx
@@ -1,34 +1,43 @@
-import { name, date, location, googleMapsLink, whatsappLink, dressCode } from '../datas/datas'
+import {
+  name,
+  date,
+  location,
+  googleMapsLink,
+  whatsappLink,
+  dressCode,
+} from "../datas/datas";
 
 export default function Invitation() {
-	return (
-		<div className="invitation-container">
-			<div className="background-overlay"></div>
+  return (
+    <div className="">
+      <div className=""></div>
 
-			<div className="card">
-				<h1 className="title">18</h1>
-				<p className="subtitle">Sei invitato/a al diciottesimo di</p>
-				<h2 className="name">{name}</h2>
-				<p className="info">📅 {date}</p>
-				<p className="info">📍 {location}</p>
-				<p className="info">🎩 Dress Code: {dressCode}</p>
-				<div className="buttons">
-					<a
-						href={whatsappLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="button">
-						✅ Conferma su WhatsApp
-					</a>
-					<a
-						href={googleMapsLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="button">
-						📍 Vedi la posizione
-					</a>
-				</div>
-			</div>
-		</div>
-	)
+      <div className="">
+        <h1 className="m-2 p-4 text-blue-500">18</h1>
+        <p className="">Sei invitato/a al diciottesimo di</p>
+        <h2 className="">{name}</h2>
+        <p className="">📅 {date}</p>
+        <p className="">📍 {location}</p>
+        <p className="">🎩 Dress Code: {dressCode}</p>
+        <div className="">
+          <a
+            href={whatsappLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            ✅ Conferma su WhatsApp
+          </a>
+          <a
+            href={googleMapsLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            📍 Vedi la posizione
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
