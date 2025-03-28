55f6495e4debf30d91bf16640a3d7f6512d43cea
claudiomavi <118379948+claudiomavi@users.noreply.github.com>
2025-03-28 11:17:01 +0000

    changed datas

--- src/datas/datas.js
+++ src/datas/datas.js
@@ -1,12 +1,11 @@
 export const name = "Rosa";
-export const date = { day: "sabato", number: 27, month: "ottobre" };
-export const location = "Ristorante La Bella Vita";
-export const time = "20:30";
-export const googleMapsLink =
-  "https://www.google.com/maps?q=Ristorante+La+Bella+Vita,+Roma";
+export const date = { day: "sabato", number: 12, month: "luglio" };
+export const location = "Karibú CLUB - Giugliano";
+export const time = "20:00";
+export const googleMapsLink = "https://maps.app.goo.gl/PJFFVmM6gyr1qK2M9";
 const phoneNumber = "393917647207"; // Código de país + número sin espacios
 const whatsappMessage = encodeURIComponent(
   `Ciao ${name}! Confermo la mia presenza alla tua festa`,
 );
 export const whatsappLink = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`;
-export const dressCode = "Tropicale";
+export const dressCode = "Accessorio Tropicale";
